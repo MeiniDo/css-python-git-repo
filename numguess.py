53125d4017d93995d38850dcfbbf9136dae21f21
MeiniDo <doming02@hanmail.net>
2017-03-29 18:28:26 +0900

    numguess.py edit

--- numguess.py
+++ numguess.py
@@ -6,11 +6,23 @@ print(answer)
 
 username = input("What is yout name? ")
 
+chance =3
+
 while True :
     guess = eval(input("Hi, " + username + ". Guess the number : "))
 
     if guess == answer :
         print("Correct")
         break
-    else :
-        print("Incorrect~~")
+    elif guess > answer :
+        chance -= 1
+        print("Too Hight. (%d times left)" % (chance))
+        if(chance == 0) :
+            print("You are dead")
+            break;
+    elif guess < answer :
+        chance -= 1
+        print("Too low. (%d times left)" % (chance))
+        if(chance == 0) :
+            print("You are dead")
+            break;
